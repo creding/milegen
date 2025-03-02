754d1103fcdd29e5d75cf879c2586f6c226fc33a
Christopher Reding <ccreding@gmail.com>
2025-03-02 00:04:25 -0600

    DELETED

--- components/homepage/index.ts
+++
@@ -1,6 +0,0 @@
-export { HeroSection } from './HeroSection';
-export { StepsSection } from './StepsSection';
-export { TestimonialsSection } from './TestimonialsSection';
-export { CTASection } from './CTASection';
-export { FinalCTA } from './FinalCTA';
-export { SEOSection } from './SEOSection';
